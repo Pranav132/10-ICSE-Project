d399b9400fdab7be0cdf67ff477861fb92936522
Pranav Iyengar <pranav1@Aditischool.org>
2017-11-10 13:03:00 +0530

    Fix a by doing the anagram letter choosing thingy

--- a.java
+++ a.java
@@ -1,13 +1,15 @@
-
+import java.util.*;
 class a {
     public static void main(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("You will now begin to try and figure out the name of the item that is being auctioned");
         System.out.println("To choose the letter you want place, type it's corresponding number");
         System.out.println("After that, type the position of where you want to place the number");
         System.out.println("A few clues:");
         System.out.println("Capital letters will always be at the start of words");
         System.out.println("The number of spaces in the shuffled phrase will determine the number of words in the name of the item.");
-        System.out.println("Remember, a space is also counted as a character. The spaces have been entered in their correct placed for you.");
+        System.out.println("Remember, a space is also counted as a character. The spaces have been entered in their correct placed for you. In it's place");
+        System.out.println("The characters which have been placed have been replaced by a '~'");
         String shuffledauctionitem = "Penguin Lamp Keyboard";
         for(int i = 0; i<shuffledauctionitem.length(); i++){
             if (i<9){
@@ -18,10 +20,21 @@ class a {
             }
         }
         System.out.println();
+        char qq;
+        String sch ="";
         for (int a = 0; a<shuffledauctionitem.length();a++){
 
-            System.out.print(shuffledauctionitem.charAt(a) + "  " );
+            int dd = shuffledauctionitem.charAt(a);
 
+            if((dd <= 90 && dd >= 65)  ||dd == 32){
+                System.out.print("~  ");
+                qq = '~';
+            }
+            else{
+                System.out.print(shuffledauctionitem.charAt(a) + "  " );
+                qq = shuffledauctionitem.charAt(a);
+            }
+            sch+= qq;
         }
         System.out.println();
         System.out.println();
@@ -34,18 +47,38 @@ class a {
             }
         }
         System.out.println();
+        char pp;
+        String scr ="";
         for(int i = 0; i<shuffledauctionitem.length(); i++){
-            if(shuffledauctionitem.charAt(i) != ' '){
-                System.out.print("_  ");
+            int cc = shuffledauctionitem.charAt(i);
+
+            if((cc <= 90 && cc >= 65)  || cc == 32){
+                System.out.print(shuffledauctionitem.charAt(i) + "  ");
+                pp  = shuffledauctionitem.charAt(i);
             }
             else{
-                System.out.print("   ");
+                System.out.print("_  ");
+                pp = '_';
             }
+            scr+=pp;
         }
         System.out.println();
         System.out.println();
         
-    }
-    
+        System.out.println("Enter 1 for choosing letter from anagram, enter 2 for letter from the solution");
 
+        while(true){
+            System.out.println("Enter the corresponding number of the letter you want to move from the anagram");
+            System.out.println("or enter the corresponding number of the letter you want to send back to it's place in the anagram. ");
+            int r = sc.nextInt();
+            if (sch.charAt(r - 1) == '~'){
+                System.out.println("Character has already been placed.");
+                // || scr.charAt(r - 1) == '_'
+            }
+            else{
+                break;
+            }
+        }
+
+    }
 }
---
+++ skshs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class skshs{
+    public static void main (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter");
+        char n = sc.next().charAt(0);
+        int cc = n;
+        System.out.print(cc);
+    }
+}
